9ba1d6be7e2dd2ced01ce38a725b19de62bc8089
Daniel Mora <dmora@spark451.com>
2017-01-05 16:49:21 -0600

    Create new structure

--- index.php
+++
@@ -1,9 +0,0 @@
-<?php
-include_once 'SearchWords.php';
-
-$string = file_get_contents('text.txt');
-
-$searchWords = new SearchWords($string, 10);
-$result = $searchWords->getRepeatedWords();
-
-echo '<pre>' . print_r($result, 1) . '</pre>';
